6f555252b61060e72bcf0f632a5e1818485455d4
Cameron Stark <starkc920@outlook.com>
2019-01-17 20:40:30 -0500

    added new app

---
+++ practice/lib/pages/quiz_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../UI/answer_button.dart';
+import '../UI/question_text.dart';
+import '../UI/correct_wrong_overlay.dart';
+
+import '../utils/questions.dart';
+import '../utils/quiz.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Elon Musk is human", false),
+    new Question("Pizza is healthy", false),
+    new Question("Flutter is awesome", true)
+  ]);
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayShouldBeVisible = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        new Column(
+          children: <Widget>[
+            new AnswerButton(true, () => handleAnswer(true)),
+            new QuestionText("Cool", 1),
+            new AnswerButton(false, () => handleAnswer(false))
+          ],
+        ),
+        overlayShouldBeVisible == true ? new CorrectWrongOverlay(
+          isCorrect, 
+          () {
+            currentQuestion = quiz.nextQuestion;
+            this.setState(() {
+              overlayShouldBeVisible = false;
+              questionText = currentQuestion.question;
+              questionNumber = quiz.questionNumber;
+            });
+          }
+        ) : new Container()
+      ],
+    );
+  }
+}
